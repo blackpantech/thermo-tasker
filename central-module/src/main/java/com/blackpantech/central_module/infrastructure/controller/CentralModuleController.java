bd31f6bfebc28e4185163f3c32f3e63546e76da3
blackpantech <pantech@blackpantech.com>
2025-11-12 21:58:23 +0100

    feature(central-module): constants for view names

--- central-module/src/main/java/com/blackpantech/central_module/infrastructure/controller/CentralModuleController.java
+++ central-module/src/main/java/com/blackpantech/central_module/infrastructure/controller/CentralModuleController.java
@@ -4,7 +4,6 @@ import com.blackpantech.central_module.application.TaskService;
 import com.blackpantech.central_module.domain.Task;
 import com.blackpantech.central_module.domain.exceptions.TaskPersistenceException;
 import com.blackpantech.central_module.domain.exceptions.TaskQueueingException;
-import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -13,8 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.Instant;
+
 @Controller
 public class CentralModuleController {
+  private static final String NEW_VIEW = "new";
+  private static final String INDEX_VIEW = "index";
+  private static final String TASKS_VIEW = "tasks";
+  private static final String REDIRECT_INDEX_VIEW = "redirect:/";
   private final TaskService taskService;
   private final Logger logger = LoggerFactory.getLogger(CentralModuleController.class);
 
@@ -25,7 +30,7 @@ public class CentralModuleController {
   @GetMapping
   public String getHomePage() {
     logger.debug("Serving home page.");
-    return "index";
+    return INDEX_VIEW;
   }
 
   @GetMapping("/new")
@@ -33,7 +38,7 @@ public class CentralModuleController {
     TaskForm newTaskForm = new TaskForm("", "");
     model.addAttribute("newTaskForm", newTaskForm);
     logger.debug("Serving task creation page.");
-    return "new";
+    return NEW_VIEW;
   }
 
   @GetMapping("/tasks")
@@ -41,7 +46,7 @@ public class CentralModuleController {
     logger.debug("Getting all current tasks.");
     model.addAttribute("tasks", taskService.getTasks());
     logger.debug("Serving current tasks list page.");
-    return "tasks";
+    return TASKS_VIEW;
   }
 
   @PostMapping("/tasks")
@@ -50,7 +55,7 @@ public class CentralModuleController {
     if (newTaskForm.topic().isBlank() || newTaskForm.description().isBlank()) {
       logger.error("Task's topic and description cannot be empty.");
       model.addAttribute("errorMessage", "Task's topic and description cannot be empty.");
-      return "new";
+      return NEW_VIEW;
     }
     var newTask = new Task(newTaskForm.topic(), newTaskForm.description(), Instant.now());
     logger.debug(
@@ -70,7 +75,7 @@ public class CentralModuleController {
           String.format(
               "Could not queue task with topic \"%s\", description \"%s\" and due date %s.",
               newTask.topic(), newTask.description(), newTask.dueDate()));
-      return "new";
+      return NEW_VIEW;
     } catch (TaskPersistenceException exception) {
       logger.error(
           "Could not persist task with topic \"{}\", description \"{}\" and due date {}.",
@@ -82,9 +87,9 @@ public class CentralModuleController {
           String.format(
               "Could not persist task with topic \"%s\", description \"%s\" and due date %s.",
               newTask.topic(), newTask.description(), newTask.dueDate()));
-      return "new";
+      return NEW_VIEW;
     }
     logger.debug("Redirecting to home page.");
-    return "redirect:/";
+    return REDIRECT_INDEX_VIEW;
   }
 }
