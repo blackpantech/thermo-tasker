ff9e812123aa543d9ae9fce3b16c4e2ef33bef57
blackpantech <pantech@blackpantech.com>
2025-11-18 17:24:30 +0100

    feat(printer-module): logs

--- printer-module/src/main/java/com/blackpantech/printer_module/infrastructure/repository/JpaTaskRepository.java
+++ printer-module/src/main/java/com/blackpantech/printer_module/infrastructure/repository/JpaTaskRepository.java
@@ -1,11 +1,15 @@
 package com.blackpantech.printer_module.infrastructure.repository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.blackpantech.printer_module.domain.Task;
 import com.blackpantech.printer_module.domain.ports.TaskRepository;
 
 public class JpaTaskRepository implements TaskRepository {
 
   private final TaskJpaRepository taskJpaRepository;
+  private final Logger logger = LoggerFactory.getLogger(JpaTaskRepository.class);
 
   public JpaTaskRepository(TaskJpaRepository taskJpaRepository) {
     this.taskJpaRepository = taskJpaRepository;
@@ -13,11 +17,23 @@ public class JpaTaskRepository implements TaskRepository {
 
   @Override
   public void markTaskAsSuccessfullyPrinted(Task task) {
+    logger.debug(
+          "Updating task {} with topic \"{}\", description \"{}\" and due date \"{}\" as successfully printed.",
+          task.id(),
+          task.topic(),
+          task.description(),
+          task.dueDate());
     taskJpaRepository.updatePrintingStatus(task.id(), PrintingStatus.SUCCESS);
   }
 
   @Override
   public void markTaskAsFailedToPrint(Task task) {
+    logger.debug(
+          "Updating task {} with topic \"{}\", description \"{}\" and due date \"{}\" as failed to print.",
+          task.id(),
+          task.topic(),
+          task.description(),
+          task.dueDate());
     taskJpaRepository.updatePrintingStatus(task.id(), PrintingStatus.FAILED);
   }
 
