85480260bf450ee72b684d58397594850f04b3fd
blackpantech <pantech@blackpantech.com>
2026-02-04 12:13:03 +0100

    feat(central-module): Spring Scheduler job implementation

--- central-module/src/main/java/com/blackpantech/central_module/infrastructure/scheduler/SendDueTasksJob.java
+++ central-module/src/main/java/com/blackpantech/central_module/infrastructure/scheduler/SendDueTasksJob.java
@@ -1,8 +1,11 @@
 package com.blackpantech.central_module.infrastructure.scheduler;
 
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import com.blackpantech.central_module.domain.Task;
+import com.blackpantech.central_module.domain.exceptions.TaskQueueingException;
 import com.blackpantech.central_module.domain.ports.TaskMessageBroker;
 import com.blackpantech.central_module.domain.ports.TaskRepository;
 
@@ -21,6 +24,15 @@ public class SendDueTasksJob {
 
   @Scheduled(cron = "0 * * * * *")
   public void sendDueTasksJob() {
-    logger.info("Oh, I'm running! A new minute has started!");
+    logger.info("Fetching all due tasks.");
+    List<Task> dueTasks = taskRepository.getDueTasks();
+    logger.info("Sending due tasks to message broker.");
+    dueTasks.forEach(task -> {
+      try {
+        taskMessageBroker.sendTask(task);
+      } catch (TaskQueueingException taskQueueingException) {
+        logger.error("Error while sending task to message broker.", taskQueueingException);
+      }
+    });
   }
 }
